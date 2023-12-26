8405214c0f0eb000ab68fc2a2b271f94b99a550a
Faithful <faithfuleromosele001@gmail.com>
2023-12-26 21:28:39 +0100

    done!

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,28 +12,28 @@ const NavBar = () => {
 	return (
 		<div className="navbar px-10 pt-[85px] pb-5 z-20 fixed w-screen">
 			<div className="section flex justify-between">
-				<div className="flex items-center">
-					<img src={vulue_logo} alt="" className="h-7 mr-2" />
-					<p>VULUE</p>
+				<div className="flex items-center z-10">
+					<img src={vulue_logo} alt="" className="h-7 mr-1" />
+					<p className="font-semibold">VULUE</p>
 				</div>
 				<div
 					className={`nav-text flex transition-all duration-500 ease-in ${
 						!isOpen ? "-right-[300px]" : "right-0"
 					}`}
 				>
-					<a href="#" className="text-xs ml-5" onClick={() => setIsOpen(false)}>
+					<a href="" className="text-xs ml-5" onClick={() => setIsOpen(false)}>
 						Account Settings
 					</a>
-					<a href="#" className="text-xs ml-5" onClick={() => setIsOpen(false)}>
+					<a href="" className="text-xs ml-5" onClick={() => setIsOpen(false)}>
 						Dashboard
 					</a>
-					<a href="#" className="text-xs ml-5" onClick={() => setIsOpen(false)}>
+					<a href="" className="text-xs ml-5" onClick={() => setIsOpen(false)}>
 						Our Offering
 					</a>
-					<a href="#" className="text-xs ml-5" onClick={() => setIsOpen(false)}>
+					<a href="" className="text-xs ml-5" onClick={() => setIsOpen(false)}>
 						Investor Relations
 					</a>
-					<a className="text-xs ml-5" onClick={() => setIsOpen(false)}>
+					<a href="" className="text-xs ml-5" onClick={() => setIsOpen(false)}>
 						Contact Us
 					</a>
 					<img
@@ -43,7 +43,7 @@ const NavBar = () => {
 					/>
 				</div>
 				<span
-					className="material-symbols-outlined cursor-pointer md:hidden text-[#455A64]"
+					className="material-symbols-outlined cursor-pointer md:hidden block z-10 text-[#455A64]"
 					onClick={handleNavBar}
 				>
 					menu
